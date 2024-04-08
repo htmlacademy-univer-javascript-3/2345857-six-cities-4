1893184ce28397378eecc34cc1a7cff60a1d16ae
Arseniy Graur <me@argraur.dev>
2024-04-08 23:09:54 +0300

    module3-task1: Implement routing

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,27 @@
 import { FC } from 'react';
 import { MainPage } from './pages/main/main-page';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { LoginPage } from './pages/auth/login-page';
+import { OfferPage } from './pages/offers/offer-page';
+import { PrivateRoute } from './components/private-route';
+import { FavoritesPage } from './pages/favorites/favorites-page';
+import { AppRoute } from './types/app-route';
+import { NotFoundPage } from './pages/service/not-found-page';
 
-export const App: FC = () => <MainPage offers={5} />;
+export const App: FC = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route path={AppRoute.Index} element={<MainPage offers={5} />} />
+      <Route path={AppRoute.Login} element={<LoginPage />} />
+      <Route path={AppRoute.Offer} element={<OfferPage/>} />
+      <Route path={AppRoute.Favorites} element={
+        <PrivateRoute user={null}>
+          <FavoritesPage />
+        </PrivateRoute>
+      }
+      />
+      <Route path="*" element={<NotFoundPage />}/>
+    </Routes>
+  </BrowserRouter>
+);
+
