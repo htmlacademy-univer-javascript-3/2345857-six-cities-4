a7e44c8bada21c351bfb6ebae2dced48af02212d
Arseniy Graur <me@argraur.dev>
2024-05-31 01:43:07 +0300

    Use tanstack query

--- src/app.tsx
+++ src/app.tsx
@@ -10,7 +10,10 @@ import { NotFoundPage } from './pages/service/not-found-page';
 import { MainLayout } from './layouts/main-layout';
 import { useAppDispatch } from './store/helpers';
 import { fetchAuthAsync, fetchFavoritesAsync, fetchOffersAsync } from './store/action';
+import { CITIES } from './const';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
+const queryClient = new QueryClient();
 
 export const App: FC = () => {
   const dispatch = useAppDispatch();
@@ -22,31 +25,34 @@ export const App: FC = () => {
   ].forEach(dispatch);
 
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path={AppRoute.Index} element={
-          <MainLayout color="gray">
-            <MainPage />
-          </MainLayout>
-        }
-        />
-        <Route path={AppRoute.Login} element={<LoginPage />} />
-        <Route path={AppRoute.Offer} element={
-          <MainLayout>
-            <OfferPage />
-          </MainLayout>
-        }
-        />
-        <Route path={AppRoute.Favorites} element={
-          <PrivateRoute>
-            <MainLayout>
-              <FavoritesPage />
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route path={AppRoute.Index} element={
+            <MainLayout color="gray">
+              <MainPage />
             </MainLayout>
-          </PrivateRoute>
-        }
-        />
-        <Route path="*" element={<NotFoundPage />}/>
-      </Routes>
-    </BrowserRouter>
+          }
+          />
+          <Route path={AppRoute.Login} element={<LoginPage cities={CITIES} />} />
+          <Route path={AppRoute.Offer} element={
+            <MainLayout>
+              <OfferPage />
+            </MainLayout>
+          }
+          />
+          <Route path={AppRoute.Favorites} element={
+            <PrivateRoute>
+              <MainLayout>
+                <FavoritesPage />
+              </MainLayout>
+            </PrivateRoute>
+          }
+          />
+          <Route path="*" element={<NotFoundPage />}/>
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
+
   );
 };
