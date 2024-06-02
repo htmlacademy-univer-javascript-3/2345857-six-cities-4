7cbee3edb0c6ecf7c7c3e172912275fdbfa34cad
Arseniy Graur <me@argraur.dev>
2024-06-02 15:51:29 +0300

    Replace all <a> tags with routers

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,8 @@
 import { FC } from 'react';
-import { MainPage } from './pages/main/main-page';
+import { MainPage, LoginPage, OfferPage, FavoritesPage, NotFoundPage } from './pages/index';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { LoginPage } from './pages/auth/login-page';
-import { OfferPage } from './pages/offers/offer-page';
 import { PrivateRoute } from './components/private-route';
-import { FavoritesPage } from './pages/favorites/favorites-page';
 import { AppRoute } from './types/app-route';
-import { NotFoundPage } from './pages/service/not-found-page';
 import { MainLayout } from './layouts/main-layout';
 import { useAppDispatch } from './store/helpers';
 import { fetchAuthAsync, fetchFavoritesAsync, fetchOffersAsync } from './store/action';
